38c2ed2d12bcc37c7604f846beec3dd4f8ca4157
goodplayer <meidomx@gmail.com>
2017-04-10 23:00:05 +0800

    bubble sort

--- go/basic_algorithm/bubble_sort/bubble.go
+++ go/basic_algorithm/bubble_sort/bubble.go
@@ -4,10 +4,10 @@ func BubbleSort(data []int) []int {
 	length := len(data)
 	for i := 0; i < length; i++ {
 		for j := length - 1; j > i; j-- {
-			if data[j - 1] > data[j] {
+			if data[j-1] > data[j] {
 				temp := data[j]
-				data[j] = data[j - 1]
-				data[j - 1] = temp
+				data[j] = data[j-1]
+				data[j-1] = temp
 			}
 		}
 	}
